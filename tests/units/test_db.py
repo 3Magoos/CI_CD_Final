9e26a88deafac73c9b6da1de1cb35d65e353eff9
freddeVR <fredrik.barre@yh.nackademin.se>
2022-12-01 10:17:22 +0100

    Update test_db.py

--- tests/units/test_db.py
+++ tests/units/test_db.py
@@ -5,11 +5,3 @@ def test_db_conf():
 
 def test_db_conf_info():
     assert get_db_config() != ""
-
-def test_db_conf_values():
-    values = ["127.0.0.1", "username", None, "example"]
-    assert list(get_db_config().values()) == values
-
-def test_db_conf_keys():
-    keys = ["host", "user", "password", "database"]
-    assert list(get_db_config().keys()) == keys
