b539cca9cc70e9996b97bfcace6da11ada296bd1
4mago <4mago@github.com>
2022-12-01 11:04:04 +0100

    testing

---
+++ tests/test_flask.py
@@ -0,0 +1,18 @@
+import pytest
+from shop_app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_request_example(client):
+    response = client.get("/")
+    assert isinstance(response.text, str)
